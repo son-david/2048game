b0e785cf2ab6026d8b64cca6e55791bd913f8851
son-david <sondavid92@gmail.com>
2016-04-01 16:36:27 -0700

    basic setup

--- app.js
+++ app.js
@@ -2,8 +2,28 @@ var app = angular.module('app',[]);
 
 app.controller('controller', function ($scope){
 
-  $scope.test = function () {
-    console.log('testttt');
+  $scope.tiles = []
+
+  for (var i =0; i < 4 ; i++ ){
+    $scope.tiles.push(new Tile(0,i,1));
+  }
+  for (var i =0; i < 4 ; i++ ){
+    $scope.tiles.push(new Tile(1,i,1));
   }
 
+
+  
+
+
+
 });
+
+
+function Tile (r,c,v) {
+  this.row = r;
+  this.column = c;
+  this.value = v;
+  this.class = function () {
+    return 'r' + this.row + ' c' + this.column;
+  }
+}
